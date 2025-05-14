f8fff89918ce137e113cb4f102414c25925a98f4
uroma <andrewurom@gmail.com>
2025-05-14 20:58:45 +0100

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.resumexpert.info",
+  generateRobotsTxt: true, // Optional
+  sitemapSize: 7000, // Optional
+};
