a30ed36a14ffd46e4ebafd7920b7a150378bc6ba
uroma <andrewurom@gmail.com>
2025-05-14 21:15:52 +0100

    added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.resumexpert.info",
-  generateRobotsTxt: true, // Optional
-  sitemapSize: 7000, // Optional
+  generateRobotsTxt: true,
+  // optional: split into multiple files if > 50k URLs
+  sitemapSize: 50000,
 };
